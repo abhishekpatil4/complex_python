e971ebb35ae02efd568011e6f7028e8092e50510
Athar Wani <83573286+WaniAthar@users.noreply.github.com>
2022-10-06 23:08:08 +0530

    Update stdio.py

--- modules/stdio.py
+++ modules/stdio.py
@@ -1,24 +1,14 @@
 """custom io module created by athar mujtaba wani"""
-
-def printf(a):
-    try:
-        print(a)
-    except Exception as e:
-        print(e)
-
 def scanf(str:str):
     input()
-
-def scanf(int:int):
     try:
         int(input())
     except Exception as e:
-        print(e)
+        
+        try:
+            float(input())
+        except Exception as e:
+            print(e)
 
-def scanf(float:float):
-    try:
-        float(input())
-    except Exception as e:
-        print(e)
 
 # more functions to be added
